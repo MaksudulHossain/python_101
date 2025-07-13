d7ddedb86068593f921bd8d1407689945f3bcb93
MaksudulHossain <jewelmaksud@gmail.com>
2025-07-13 08:22:26 -0700

    setting class seperate

--- python_game_project/Alien_Invasion_maksud/alien_invasion.py
+++ python_game_project/Alien_Invasion_maksud/alien_invasion.py
@@ -9,6 +9,11 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((800,600))
         pygame.display.set_caption('Alien Invasion')
 
+        #set bg 
+        self.bg_color = (230,230,230)
+
+
+
     def run_game(self):
 
         while True:
@@ -16,8 +21,11 @@ class AlienInvasion:
                 if even.type == pygame.QUIT:
                     sys.exit() 
 
+            # redraw 
+            self.screen.fill(self.bg_color)
+
             pygame.display.flip()
 
-if __name__ == '__main':
+if __name__ == '__main__':
     ai = AlienInvasion()
     ai.run_game()
